fbd4befc03678b73fcf2b4dce4f8ba62846208f3
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2021-07-09 03:04:54 +0300

    toBool done

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+type falsy = 0 | "" | '' | `` | false | -0 | null | undefined | typeof NaN;
+
+const toBool: (value: any, exceptFalsy: falsy) => boolean = (value, exceptFalsy) => (!!value) || (value === exceptFalsy) || (Number.isNaN(exceptFalsy) && (Number.isNaN(value) === Number.isNaN(exceptFalsy)));
+
+export default toBool;
