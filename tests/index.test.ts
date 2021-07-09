08452f6fbc46b6d59b4e2c36faefface6e8ea826
Oguzhan Ince <oguzhan824@gmail.com>
2021-07-10 00:46:46 +0300

    REFACTORED: Added 'when' block to all cases

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,67 +1,93 @@
-import {toBool} from "../src"
+import { toBool } from '../src';
 
 describe('toBool()', () => {
-    it("value: 0 and except value: 0 should true", () => {
+  describe('when value equals to 0 and expected value equals to 0', () => {
+    it('returns true ', () => {
       expect(toBool(0, 0)).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: NaN and except value: NaN should true", () => {
+  describe('when value equals to NaN and expected value equals to NaN', () => {
+    it('returns true ', () => {
       expect(toBool(NaN, NaN)).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: false and except value: false should true", () => {
+  describe('when value equals to false and expected value equals to false', () => {
+    it('returns true ', () => {
       expect(toBool(false, false)).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: null and except value: null should true", () => {
+  describe('when value equals to null and expected value equals to null', () => {
+    it('returns true ', () => {
       expect(toBool(null, null)).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: undefined and except value: undefined should true", () => {
+  describe('when value equals to undefined and expected value equals to undefined', () => {
+    it('returns true ', () => {
       expect(toBool(undefined, undefined)).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: -0 and except value: -0 should true", () => {
+  describe('when value equals to negative 0 and expected value equals to negative 0', () => {
+    it('returns true ', () => {
       expect(toBool(-0, -0)).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: \"\" and except value: \"\" should true", () => {
-      expect(toBool("", "")).toBeTruthy();
-    })
-
-    it("value: '' and except value: '' should true", () => {
+  describe('when value equals to empty string and expected value equals to empty string', () => {
+    it('returns true ', () => {
       expect(toBool('', '')).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: `` and except value: `` should true", () => {
+  describe('when value equals to back tick and expected value equals to back tick', () => {
+    it('returns true ', () => {
       expect(toBool(``, ``)).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: 1 and except value: 0 should true", () => {
+  describe('when value equals to 1 and expected value equals to 0', () => {
+    it('returns true ', () => {
       expect(toBool(1, 0)).toBeTruthy();
-    })
+    });
+  });
 
-    it("value: NaN and except value: 0 should false", () => {
+  describe('when value equals to NaN and expected value equals to 0', () => {
+    it('returns false ', () => {
       expect(toBool(NaN, 0)).toBeFalsy();
-    })
+    });
+  });
 
-    it("value: 0 and except value: false should false", () => {
+  describe('when value equals to 0 and expected value equals to false', () => {
+    it('returns false ', () => {
       expect(toBool(0, false)).toBeFalsy();
-    })
+    });
+  });
 
-    it("value: undefined and except value: null should false", () => {
+  describe('when value equals to undefined and expected value equals to null', () => {
+    it('returns false ', () => {
       expect(toBool(undefined, null)).toBeFalsy();
-    })
+    });
+  });
 
-    it("value: \"\" and except value: `` should true because \"\" === `` true", () => {
-      expect(toBool("", ``)).toBeTruthy();
-    })
+  describe('when value equals to empty string and expected value equals to back tick', () => {
+    it('returns true ', () => {
+      expect(toBool('', ``)).toBeTruthy();
+    });
+  });
 
-    it("value: -0 and except value: '' should false", () => {
+  describe('when value equals to negative 0 and expected value equals to empty string', () => {
+    it('returns false ', () => {
       expect(toBool(-0, '')).toBeFalsy();
-    })
+    });
+  });
 
-    it("value: true and except value: 0 should false", () => {
+  describe('when value equals to true and expected value equals to 0', () => {
+    it('returns false ', () => {
       expect(toBool(true, 0)).toBeTruthy();
-    })
-})
+    });
+  });
+});
